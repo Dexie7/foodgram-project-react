ee77c091a18affd90e28fb92cf0079993ca8a410
dexie <dexie7@yande.ru>
2022-11-12 18:34:09 +0500

    My 8 commit

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -29,7 +29,7 @@ class RecipeAdmin(AdminImageMixin, admin.ModelAdmin):
     inlines = (IngredientRecipeRelationAdminInline,)
 
     fieldsets = (
-        ('Основнвые данные', {
+        ('Основные данные', {
             'fields': (
                 'name', 'author', 'image', 'tags'
             )
