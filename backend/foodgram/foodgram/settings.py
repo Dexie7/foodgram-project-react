616eb64a78e3de4a17852797287695047b49309d
dexie <dexie7@yande.ru>
2022-11-18 23:41:29 +0500

    My 16 commit

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -7,7 +7,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY', default='very-secret-key@#RFesf')
 
-DEBUG = int(os.environ.get('DEBUG', '0'))
+DEBUG = True
+# DEBUG = int(os.environ.get('DEBUG', '0'))
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = os.environ.get(
